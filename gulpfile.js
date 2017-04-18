2dfb1948e2491e7875b2f81c4c3538f460d66d30
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-04-18 18:12:08 -0400

    Rename/update files

--- gulpfile.js
+++ gulpfile.js
@@ -8,42 +8,73 @@ var ghPages = require('gulp-gh-pages');
 var babel = require('gulp-babel');
 var maps = require('gulp-sourcemaps');
 var del = require('del');
+var useref = require('gulp-useref');
+var iff = require('gulp-if');
+var csso = require('gulp-csso');
+
+// For future reference/use
+
+// gulp.task('js', function () {
+//   return gulp.src('src/js/*.js')
+//     .pipe(maps.init())
+//     .pipe(babel())
+//     .pipe(concat('all.js'))
+//     .pipe(uglify())
+//     .pipe(rename('all.min.js'))
+//     .pipe(maps.write('../maps'))
+//     .pipe(gulp.dest('dist/js'));
+// });
 
 
-gulp.task('js', function () {
-  return gulp.src('js/*.js')
-    .pipe(maps.init())
-    .pipe(babel())
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(rename('all.min.js'))
-    .pipe(maps.write('../maps'))
+// gulp.task('css', function () {
+//   return gulp.src('src/css/*.css')
+//     .pipe(minify())
+//     .pipe(rename('all.min.css'))
+//     .pipe(gulp.dest('dist/css'));
+// });
+
+
+// gulp.task('watch', function () {
+//   gulp.watch('src/js/*.js', ['js']);
+//   gulp.watch('src/css/*.css', ['css']);
+// });
+
+
+gulp.task('html', function () {
+  return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(iff('*.js', babel()))
+    .pipe(iff('*.js', uglify()))
+    .pipe(iff('*.css', csso()))
     .pipe(gulp.dest('dist'));
 });
-
-
-gulp.task('css', function () {
-  return gulp.src('css/*.css')
-    .pipe(minify())
-    .pipe(rename('all.min.css'))
-    .pipe(gulp.dest('dist'));
-});
-
-
-gulp.task('build', ['js', 'css'], function () {
-  console.log('The CSS and JS files have been updated.');
-});
-
-
-gulp.task('watch', function () {
-  gulp.watch('js/*.js', ['js']);
-  gulp.watch('css/*.css', ['css']);
-});
-
-
-gulp.task('default', ['build']); // Single Gulp task "builds" site
 
 
 gulp.task('clean', function () {
-  del('dist');
+  return del('dist');
+});
+
+
+gulp.task('assets', function(){
+  return gulp.src(['src/img/**/*',
+                   'src/slick/slick.css',
+                   'src/slick/slick.min.js',
+                  ], {base: './src'})
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('build', ['assets', 'html'], function () {
+  console.log('Clean build: Finished');
+});
+
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+}); // Single Gulp task "builds" site
+
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({branch: 'master'}));
 });
